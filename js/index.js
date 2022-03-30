1b2a1acb449293e25b6e40fdfe886dca747c637a
diogo silva <diogosilvastz@gmail.com>
2022-03-30 05:36:39 -0300

    Inserido o jquery no código

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
+$(document).ready(function() {
+
+    let slideAtual = 1;
+    let listaSlides = ["banner-1", "banner_2", "banner_3"]
+
+    setInterval(mudarSlide, 1500)
+    console.log(slideAtual)
+
+    function mudarSlide() {
+
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass(listaSlides[(listaSlides.length) - 1])
+        }
+
+        $("#carrossel").addClass(listaSlides[slideAtual]);
+
+        slideAtual++
+
+        if (slideAtual > listaSlides.length - 1) {
+            slideAtual = 0
+        }
+    }
+})
+
 function mostrarMenu() {
     let menu = document.getElementById("menu")
 
